f824b32e650a8de7d7c5cfb9f330476c3186a2f1
Kaltura CSI <community@kaltura.com>
2017-09-05 06:45:48 -0400

    New clientlib ver

---
+++ KalturaESearchService.js
@@ -0,0 +1,60 @@
+
+/**
+ *Class definition for the Kaltura service: eSearch.
+ **/
+var KalturaESearchService = {
+	/**
+	 * .
+	 * @param	searchItem	KalturaESearchItem		 (optional)
+	 **/
+	getAllowedSearchTypes: function(searchItem){
+		var kparams = new Object();
+		kparams.searchItem = searchItem;
+		return new KalturaRequestBuilder("elasticsearch_esearch", "getAllowedSearchTypes", kparams);
+	},
+	
+	/**
+	 * .
+	 * @param	searchParams	KalturaESearchParams		 (optional)
+	 * @param	pager	KalturaPager		 (optional, default: null)
+	 **/
+	searchCategory: function(searchParams, pager){
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		kparams.searchParams = searchParams;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("elasticsearch_esearch", "searchCategory", kparams);
+	},
+	
+	/**
+	 * .
+	 * @param	searchParams	KalturaESearchParams		 (optional)
+	 * @param	pager	KalturaPager		 (optional, default: null)
+	 **/
+	searchEntry: function(searchParams, pager){
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		kparams.searchParams = searchParams;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("elasticsearch_esearch", "searchEntry", kparams);
+	},
+	
+	/**
+	 * .
+	 * @param	searchParams	KalturaESearchParams		 (optional)
+	 * @param	pager	KalturaPager		 (optional, default: null)
+	 **/
+	searchUser: function(searchParams, pager){
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		kparams.searchParams = searchParams;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("elasticsearch_esearch", "searchUser", kparams);
+	}
+}
